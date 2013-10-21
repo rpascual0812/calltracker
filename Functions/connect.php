4e95ccea8e780131516bf49b12a22d7cc15c0ded
Rafael Pascual <rpascual0812@gmail.com>
2013-10-21 21:12:20 +0800

    added new backup, changed connect

--- Functions/connect.php
+++ Functions/connect.php
@@ -1,4 +1,4 @@
 <?php
-pg_connect("host=ec2-54-235-86-117.compute-1.amazonaws.com port=5432 dbname=d874rvdu121c9e user=ucvmphdjmdligt password=aKAslLupdFNitIHrea9D5SC2HU sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect("host=ec2-54-235-102-202.compute-1.amazonaws.com port=5432 dbname=dbch86uits0ucs user=dxbnuxzfuzyayg password=IJFDTXeHWyE8eSZf4bNfDLev5U sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 //pg_connect("host=localhost dbname=acq_reception user=reception password=reception");
 ?>
